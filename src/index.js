ecc6e0d903bc968660ef386e2bc2ea5367f500ad
mohammad azim <mohammad@mohammad-azim-pc.scoiumtech.local>
2022-10-24 12:55:10 +0300

    add: isThereWinner function

--- src/index.js
+++ src/index.js
@@ -25,10 +25,37 @@ class Board extends React.Component {
         };
     }
 
+    calculateWinner(squares){
+        const lines = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,4,8],
+            [2,4,6],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+        ]
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a,b,c] = lines[i];
+            
+            if (squares[a] == squares[b] && squares[a] == squares[c] ){
+                return squares[a]
+            }
+        }
+        return null
+    }
+
     handleClick(i){
         const squares = this.state.squares.slice();
+        const isThereWinner =this.calculateWinner(squares);
+        if (isThereWinner){
+            return
+        }
         squares[i] = this.state.xIsNext ? "X" : "O";
         this.setState({squares: squares , xIsNext : !this.state.xIsNext});
+        
     }
 
     renderSquare(i) {
@@ -40,7 +67,12 @@ class Board extends React.Component {
     }
 
     render() {
-        const status = 'Next player: '+ (this.state.xIsNext ? "X" : "O");
+        let status = 'Next player: '+ (this.state.xIsNext ? "X" : "O");
+        const isWinner = this.calculateWinner(this.state.squares)
+        if(isWinner){
+            status = "winner is " + isWinner
+        }
+
 
         return (
             <>
